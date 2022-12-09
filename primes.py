a65c6d15c8d34a50ad06ad2b89369f92c94eae7d
Samar7z <101511093+Samar7z@users.noreply.github.com>
2022-12-09 12:47:11 +0000

    Update primes.py

--- primes.py
+++ primes.py
@@ -3,4 +3,22 @@
 
 def primes(number_of_primes):
     list = []
+    if number_of_primes <= 0:
+        raise ValueError("Value should be more than 0")
+
+    else:
+        num = 2
+        counter = 0
+
+        while ctr < number_of_primes:
+            is_Prime = True
+            for i in range(2, num):
+                if num % i == 0:
+                    is_Prime = False
+
+            if is_Prime:
+                list.append(num)
+                counter +=1
+            
+            num = num + 1
     return list
